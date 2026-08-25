6edac9301565fed4c2b390c3a13f2a3b32257bb8
HiRan <136572766+IAmYoursRey@users.noreply.github.com>
2026-08-25 19:10:43 +0700

    Delete scratch directory

--- scratch/format-specificity.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let css = fs.readFileSync('src/index.css', 'utf8');
-css = css.replace(/\.theme-([a-z]+) {/g, ':root.theme-$1 {');
-fs.writeFileSync('src/index.css', css);
--- scratch/format-tailwind.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let cfg = fs.readFileSync('tailwind.config.js', 'utf8');
-cfg = cfg.replace(/hsl\(var\(--brand-(\d+)\)\s*\/\s*<alpha-value>\)/g, 'hsla(var(--brand-$1), <alpha-value>)');
-fs.writeFileSync('tailwind.config.js', cfg);
